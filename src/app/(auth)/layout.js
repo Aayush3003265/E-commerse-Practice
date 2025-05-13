9d3d9877f125074f113db995f11f486c30cedbcb
Aayush3003265 <Aayushadhikari70@gmail.com>
2025-05-13 22:16:07 +0545

    react hook form implementation

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,11 +1,29 @@
+import Image from "next/image";
 import React from "react";
+import bg from "@/app/assets/images/login-bg.jpg";
+import cartPng from "@/app/assets/images/cart.jpg";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div>
-      Auth Page
-      {children}
-    </div>
+    <section className="flex justify-center px-10 ">
+      <Image
+        src={bg}
+        alt=""
+        height={2000}
+        width={2000}
+        className="h-full w-full fixed max-w-screen left-0  right-0 -z-1 object-cover"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full lg:w-2/3  xl:w-1/2 shadow-xl rounded-xl bg-white min-h-[70vh] ">
+        {children}
+        <Image
+          src={cartPng}
+          alt=""
+          height={1200}
+          width={1200}
+          className="h-full w-auto rounded-r-xl object-cover"
+        />
+      </div>
+    </section>
   );
 };
 
--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,7 +1,40 @@
+"use client";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  return <div>Login</div>;
+  const { register } = useForm();
+  const { ref, name, onChange, onBlur } = register("email");
+  return (
+    <div className="py-12 px-12">
+      <h1 className="text-center text-3xl font-bold mb-4">Login Page</h1>
+      <div className="py-2">
+        <label htmlFor="email">Email Address:</label>
+        <input
+          type="email"
+          id="email"
+          name={name}
+          ref={ref}
+          className="border rounded-xl w-full p-2 mt-1"
+        />
+      </div>
+      <div className="py-2">
+        <label htmlFor="password">password:</label>
+        <input
+          type="password"
+          id="password"
+          className="border rounded-xl w-full p-2 mt-1"
+        />
+      </div>
+      <div className="py-2">
+        <input
+          type="submit"
+          value="Login"
+          className="w-full bg-blue-600 hover:bg-blue-700 mt-2 text-white py-2 rounded hover:cursor-pointer"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
