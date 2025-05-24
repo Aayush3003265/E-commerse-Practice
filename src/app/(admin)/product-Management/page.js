5fe2a9718d3ee77d815cc36a9b94212270b620d7
Aayush3003265 <Aayushadhikari70@gmail.com>
2025-05-25 01:04:48 +0545

    made product management page with products table

--- src/app/(admin)/product-Management/page.js
+++ src/app/(admin)/product-Management/page.js
@@ -1,7 +1,14 @@
+import { getProducts } from "@/api/products";
+import ProductsTable from "@/components/products/Table";
 import React from "react";
 
-const ProductManagement = () => {
-  return <div>This is Product Management Page</div>;
+const ProductManagement = async () => {
+  const response = await getProducts();
+  return (
+    <section className="py-8 pr-5">
+      <ProductsTable products={response.data} />
+    </section>
+  );
 };
 
 export default ProductManagement;
